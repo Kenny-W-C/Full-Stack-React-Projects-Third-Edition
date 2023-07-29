e6cbf67dd7a20a30149686a111dd3fd72a356ee7
Daniel Bugl <me@omnidan.net>
2023-07-29 14:39:29 +0200

    fix: properly handle backend errors

--- ch6/services/backend/src/routes/users.js
+++ ch6/services/backend/src/routes/users.js
@@ -2,17 +2,32 @@ import { createUser, loginUser, getUserInfoById } from '../services/users.js'
 
 export default function userRoutes(app) {
   app.post('/api/v1/user/signup', async (req, res) => {
-    const user = await createUser(req.body)
-    return res.status(201).json({ username: user.username })
+    try {
+      const user = await createUser(req.body)
+      return res.status(201).json({ username: user.username })
+    } catch (err) {
+      console.error('error creating user', err)
+      return res.status(500).end()
+    }
   })
 
   app.post('/api/v1/user/login', async (req, res) => {
-    const token = await loginUser(req.body)
-    return res.status(200).send({ token })
+    try {
+      const token = await loginUser(req.body)
+      return res.status(200).send({ token })
+    } catch (err) {
+      console.error('error logging in user', err)
+      return res.status(500).end()
+    }
   })
 
   app.get('/api/v1/users/:id', async (req, res) => {
-    const userInfo = await getUserInfoById(req.params.id)
-    return res.status(200).send(userInfo)
+    try {
+      const userInfo = await getUserInfoById(req.params.id)
+      return res.status(200).send(userInfo)
+    } catch (err) {
+      console.error('error getting user info', err)
+      return res.status(500).end()
+    }
   })
 }
--- ch9/services/backend/src/routes/posts.js
+++ ch9/services/backend/src/routes/posts.js
@@ -14,39 +14,64 @@ export default function postRoutes(app) {
     const { sortBy, sortOrder, author, tag } = req.query
     const options = { sortBy, sortOrder }
 
-    if (author && tag) {
-      return res
-        .status(400)
-        .json({ error: 'query by either author or tag, not both' })
-    } else if (author) {
-      return res.json(await listPostsByAuthor(author, options))
-    } else if (tag) {
-      return res.json(await listPostsByTag(tag, options))
-    } else {
-      return res.json(await listAllPosts())
+    try {
+      if (author && tag) {
+        return res
+          .status(400)
+          .json({ error: 'query by either author or tag, not both' })
+      } else if (author) {
+        return res.json(await listPostsByAuthor(author, options))
+      } else if (tag) {
+        return res.json(await listPostsByTag(tag, options))
+      } else {
+        return res.json(await listAllPosts())
+      }
+    } catch (err) {
+      console.error('error listing posts', err)
+      return res.status(500).end()
     }
   })
 
   app.get('/api/v1/posts/:id', async (req, res) => {
     const { id } = req.params
-    const post = await getPostById(id)
-    if (post === null) return res.status(404).end()
-    return res.json(post)
+    try {
+      const post = await getPostById(id)
+      if (post === null) return res.status(404).end()
+      return res.json(post)
+    } catch (err) {
+      console.error('error getting post', err)
+      return res.status(500).end()
+    }
   })
 
   app.post('/api/v1/posts', requireAuth, async (req, res) => {
-    const post = await createPost(req.auth.sub, req.body)
-    return res.json(post)
+    try {
+      const post = await createPost(req.auth.sub, req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('error creating post', err)
+      return res.status(500).end()
+    }
   })
 
   app.patch('/api/v1/posts/:id', requireAuth, async (req, res) => {
-    const post = await updatePost(req.auth.sub, req.params.id, req.body)
-    return res.json(post)
+    try {
+      const post = await updatePost(req.auth.sub, req.params.id, req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('error updating post', err)
+      return res.status(500).end()
+    }
   })
 
   app.delete('/api/v1/posts/:id', requireAuth, async (req, res) => {
-    const { deletedCount } = await deletePost(req.auth.sub, req.params.id)
-    if (deletedCount === 0) return res.status(404).end()
-    return res.status(204).end()
+    try {
+      const { deletedCount } = await deletePost(req.auth.sub, req.params.id)
+      if (deletedCount === 0) return res.status(404).end()
+      return res.status(204).end()
+    } catch (err) {
+      console.error('error deleting post', err)
+      return res.status(500).end()
+    }
   })
 }
