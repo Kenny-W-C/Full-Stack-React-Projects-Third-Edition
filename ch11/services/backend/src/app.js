e15707fd577f683ef0409e510189b6cbacc154e5
Daniel Bugl <me@omnidan.net>
2023-08-22 17:11:02 +0200

    finalize ch11

--- ch11/services/backend/src/app.js
+++ ch11/services/backend/src/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
 
+import { optionalAuth } from './middleware/jwt.js'
+import { typeDefs, resolvers } from './graphql/index.js'
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
 import eventRoutes from './routes/events.js'
@@ -10,6 +14,23 @@ const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+apolloServer.start().then(() =>
+  app.use(
+    '/graphql',
+    optionalAuth,
+    expressMiddleware(apolloServer, {
+      context: async ({ req }) => {
+        return { auth: req.auth }
+      },
+    }),
+  ),
+)
 
 postRoutes(app)
 userRoutes(app)
