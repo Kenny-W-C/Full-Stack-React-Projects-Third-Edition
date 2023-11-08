274d97342cf7bece08c1116ad0b47ca1d8a709a5
Daniel Bugl <me@omnidan.net>
2023-11-08 21:21:11 +0100

    ch14: implement disconnecting from sockets

--- ch14/src/components/Header.jsx
+++ ch14/src/components/Header.jsx
@@ -2,19 +2,27 @@ import { Link } from 'react-router-dom'
 import jwtDecode from 'jwt-decode'
 
 import { useAuth } from '../contexts/AuthContext.jsx'
+import { useSocket } from '../contexts/SocketIOContext.jsx'
 
 import User from './User.jsx'
 
 export default function Header() {
   const [token, setToken] = useAuth()
+  const { socket } = useSocket()
 
   if (token) {
     const { sub } = jwtDecode(token)
+
+    const handleLogout = () => {
+      socket.disconnect()
+      setToken(null)
+    }
+
     return (
       <nav>
         Logged in as <User id={sub} />
         <br />
-        <button onClick={() => setToken(null)}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </nav>
     )
   }
